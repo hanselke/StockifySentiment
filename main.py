f2805821874258312491d8c76d9fa24899d4ffeb
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2020-05-28 09:04:10 +0100

    Set up main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 class StockifySentiment(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2019, 11, 26)  # Set Start Date
+        self.SetStartDate(2017, 1, 1)  # Set Start Date
         self.SetCash(100000)  # Set Strategy Cash
-        # self.AddEquity("SPY", Resolution.Minute)
+        self.data = pd.DataFrame()
+        self.etf_list = []
 
+        # Add ETFs
+        for etf in self.etf_list:
+            self.AddEquity(etf, Resolution.Minute)
+
+        # Alpha model
+        self.CustomAlphaModel = ValueAlphaModel()
+
+        # Portfolio construction model
+        self.CustomPortfolioConstructionModel = OptimisationPortfolioConstructionModel(turnover=0.05, max_wt=0.05,
+                                                                                       longshort=True)
+
+        # Execution model
+        self.CustomExecution = Execution(liq_tol=0.005)
+
+        # Schedule rebalancing
+        self.Schedule.On(self.DateRules.Every(DayOfWeek.Wednesday), self.TimeRules.BeforeMarketClose('IVV', 0),
+                         Action(self.RebalancePortfolio))
 
     def OnData(self, data):
-        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
-        '''
+        pass
 
-        # if not self.Portfolio.Invested:
-        #    self.SetHoldings("SPY", 1)
+    def RebalancePortfolio(self):
+        alpha_df = self.CustomAlphaModel.GenerateAlphaScores(self, self.data)
+        portfolio = self.CustomPortolioConstructionModel.GenerateOptimalPortfolio(self, alpha_df)
+        self.CustomExecution.ExecutePortfolio(self, portfolio)
